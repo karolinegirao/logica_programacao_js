6dd4a22b657986ab34075a848f7212adbea7b542
karolinegirao <86789094+karolinegirao@users.noreply.github.com>
2025-06-22 17:25:39 -0300

    Add files via upload

---
+++ 03_pratica03_loops/app.js
@@ -0,0 +1,42 @@
+//Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+let contador = 1;
+
+while (contador <=10){
+    console.log(contador);
+    contador ++;
+}
+
+//------------------------------------------------------------------//
+
+//Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+// let contador = 10;
+
+// while (contador >= 0){
+//     console.log(contador);
+//     contador --;
+// }
+
+//------------------------------------------------------------------//
+
+//Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, 
+// usando um loop while no console do navegador.
+
+// let numero = prompt('Informe um número para começar a conntagem refressiva:');
+
+// while (numero >=0){
+//     console.log(numero);
+//     numero--;
+// }
+
+//------------------------------------------------------------------//
+
+//Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número,
+//  usando um loop while no console do navegador.
+
+// let numero = prompt('Informe um número para começar a contagem progressiva:');
+// let contador = 0;
+
+// while (contador <=numero){
+//     console.log(contador);
+//     contador++;
+// }
